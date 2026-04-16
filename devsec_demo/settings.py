ccb5e4f7a2304438b70e63eb0fc3aaa1791920e2
12jpaul <jeanpaulniyitegeka848@gmail.com>
2026-04-16 20:48:55 +0200

    your change description

--- devsec_demo/settings.py
+++ devsec_demo/settings.py
@@ -123,6 +123,17 @@ SECURE_HSTS_PRELOAD = not DEBUG
 SESSION_COOKIE_AGE = 3600
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
+EMAIL_BACKEND = os.environ.get(
+    'EMAIL_BACKEND',
+    'django.core.mail.backends.console.EmailBackend'
+)
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'noreply@devsec-demo.com')
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
